1caec49c57890966f27da80137aa8d9881c8d61e
Phạm Phúc Lợi <105093870+phamphucloi@users.noreply.github.com>
2022-12-14 13:27:33 +0700

    study double and decimal

--- CSharp/QuestionMark/Program.cs
+++ CSharp/QuestionMark/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Phạm Phúc Lợi");
+﻿double a = 0.1;
+
+double b = 0.2;
+
+Console.WriteLine((a+b==0.3)?"Giỏi":"Ngu");
+
+float c = 0.6f;
+
+decimal d = 0.8m;
+decimal e = 0.2m;
+
+Console.WriteLine((d + e == 1) ? "Giỏi" : "Ngu");
