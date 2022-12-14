c6efc8d492bd9c48a816ca0cfc274831c0c81907
Phạm Phúc Lợi <105093870+phamphucloi@users.noreply.github.com>
2022-12-14 13:55:16 +0700

    have operator ?, ??, nullable

--- CSharp/QuestionMark/Program.cs
+++ CSharp/QuestionMark/Program.cs
@@ -2,11 +2,27 @@
 
 double b = 0.2;
 
-Console.WriteLine((a+b==0.3)?"Giỏi":"Ngu");
+//Console.WriteLine((a+b==0.3)?"Giỏi":"Ngu");
 
 float c = 0.6f;
 
+
 decimal d = 0.8m;
 decimal e = 0.2m;
+Console.WriteLine((d + e == 1m) ? "Giỏi" : "Ngu");
 
-Console.WriteLine((d + e == 1) ? "Giỏi" : "Ngu");
+//?: => ternary operator
+string? name = null;
+Nullable<int> i = null;
+
+bool? check = null;
+
+// ? nullable value type
+
+Nullable<int> length = name is not null ? name.Length : null;
+
+if (a is 10)
+{
+
+}
+
